aeebf8e23fb9b096145331b013e43af0c8d4104b
Fabien Stervinou <stervinoufabien@gmail.com>
2022-09-26 10:48:06 +0200

    <Auth> Service / create getProfile / update getToken

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,17 +1,32 @@
 import axios from 'axios';
 
 class ServerAuth {
-  constructor() {
+  constructor () {
     this.baseURL = 'http://localhost:3001/api/v1/';
   }
 
-  getToken(email, password) {
+  getToken (email, password) {
     return axios.post(`${this.baseURL}user/login`, {
       email, password
     },
     {
-      'Content-Type': 'application/json',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
+    );
+  }
+
+  getProfile (token) {
+    return axios.post(
+      `${this.baseURL}user/profile`, 
+      { token },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      }
     );
   }
 }
