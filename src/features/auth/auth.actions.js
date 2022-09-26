c049e50a7e8910e497b88abf8195106e51def2cc
Fabien Stervinou <stervinoufabien@gmail.com>
2022-09-26 17:55:30 +0200

    Actions/Reducers: doLogout and doChangeProfile

---
+++ src/features/auth/auth.actions.js
@@ -0,0 +1,72 @@
+import ServerAuth from '../../services/auth';
+import { 
+  AUTH_LOGIN_FAILED, 
+  AUTH_LOGIN_LOADING, 
+  AUTH_LOGIN_SUCCESS, 
+  AUTH_LOGOUT,
+  AUTH_PROFILE_SUCCESS,
+  AUTH_PROFILE_CHANGE
+} from './auth.reducer';
+
+const backend = new ServerAuth();
+
+export const doLoginFailed = (error) => ({
+  type: AUTH_LOGIN_FAILED,
+  payload : {
+    error : {
+      message: error.response.data.message,
+      status : error.response.data.status
+    }
+  }
+});
+
+export const doLogin = (email, password) => async (dispatch) => {
+  dispatch({ type: AUTH_LOGIN_LOADING });
+
+  try {
+    const response = await backend.getToken(email, password);
+    dispatch({ 
+      type: AUTH_LOGIN_SUCCESS, 
+      payload: { 
+        token: response.data.body.token 
+      } 
+    });
+
+    const responseProfil = await backend.getProfile(response.data.body.token);
+    dispatch({
+      type: AUTH_PROFILE_SUCCESS, 
+      payload: {
+        user: responseProfil.data.body
+      }
+    });
+  } catch (error) {
+    return dispatch(
+      doLoginFailed(error)
+    );
+  }
+};
+
+export const doLogout = () => async (dispatch) => {
+  dispatch({ type: AUTH_LOGIN_LOADING });
+  try {
+    window.localStorage.setItem('authenticated', false);
+    return dispatch({ type: AUTH_LOGOUT });
+  } catch (error) {
+    return dispatch(doLoginFailed(error));
+  }
+};
+
+export const doChangeProfile = (firstName, lastName, token) => async (dispatch) => {
+
+  try {
+    const responseProfilChange = await backend.putProfile(firstName, lastName, token);
+    dispatch({ 
+      type: AUTH_PROFILE_CHANGE,
+      payload: {
+        user: responseProfilChange.data.body
+      }
+    });
+  } catch (error) {
+    return dispatch(doLoginFailed(error));
+  }
+};
--- src/features/auth/auth.reducer.js
+++ src/features/auth/auth.reducer.js
@@ -1,12 +1,16 @@
 export const initialAuthState = {
-  token: undefined,
-  error: undefined,
-  fetching: false
+  isAuthenticated: JSON.parse(window.localStorage.getItem('authenticated')),
+  token: null,
+  error: null,
+  fetching: false,
+  user: []
 };
 
 export const AUTH_LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS';
 export const AUTH_LOGIN_FAILED = 'AUTH_LOGIN_FAILED';
 export const AUTH_LOGIN_LOADING = 'AUTH_LOGIN_LOADING';
+export const AUTH_PROFILE_SUCCESS = 'AUTH_PROFILE_SUCCESS';
+export const AUTH_PROFILE_CHANGE = 'AUTH_PROFILE_CHANGE';
 export const AUTH_LOGOUT = 'AUTH_LOGOUT';
 
 export const authReducer = (state = initialAuthState, action) => {
@@ -14,25 +18,33 @@ export const authReducer = (state = initialAuthState, action) => {
     case AUTH_LOGIN_SUCCESS:
       return {
         ...state,
+        isAuthenticated: true,
         fetching: false,
-        error: undefined,
-        token: action.paylaod
+        token: action.payload.token
       };
     case AUTH_LOGIN_FAILED: 
       return {
-        ...state,
         fetching: false,
-        error: action.payload
+        error: action.payload.error
       };
     case AUTH_LOGIN_LOADING: 
       return {
         ...state,
         fetching: true
       };
-    case AUTH_LOGOUT: 
+    case AUTH_PROFILE_SUCCESS:
       return {
-        initialAuthState
+        ...state,
+        fetching: false,
+        user: action.payload.user
       };
+    case AUTH_PROFILE_CHANGE:
+      return {
+        ...state,
+        user: action.payload.user
+      };
+    case AUTH_LOGOUT: 
+      return initialAuthState;
     
     default:
       return state;
