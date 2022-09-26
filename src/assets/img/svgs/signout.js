22b52181272987268bc022b8706b892a1da20be3
Fabien Stervinou <stervinoufabien@gmail.com>
2022-09-26 17:53:44 +0200

    Import SVGs

---
+++ src/assets/img/svgs/signout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export function SVGSignout (props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}><path fill="currentColor" d="m221.7 133.7l-42 42a8.3 8.3 0 0 1-5.7 2.3a7.7 7.7 0 0 1-3-.6a8 8 0 0 1-5-7.4v-34h-62a8 8 0 0 1 0-16h62V86a8 8 0 0 1 5-7.4a8.2 8.2 0 0 1 8.7 1.7l42 42a8.1 8.1 0 0 1 0 11.4ZM104 208H48V48h56a8 8 0 0 0 0-16H48a16 16 0 0 0-16 16v160a16 16 0 0 0 16 16h56a8 8 0 0 0 0-16Z"></path></svg>
+  );
+}
