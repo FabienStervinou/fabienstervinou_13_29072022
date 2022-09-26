43f44829907f23b5a1008bb6b5f054ddea3f7d7a
Fabien Stervinou <stervinoufabien@gmail.com>
2022-09-26 17:55:50 +0200

    Auth logic <Header>

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -2,8 +2,20 @@ import './style.scss';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from '../../assets/img/argentBankLogo.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { doLogout } from '../../features/auth/auth.actions';
+import {SVGUser} from '../../assets/img/svgs/user.js';
+import {SVGSignout} from '../../assets/img/svgs/signout.js';
 
-function Header() {
+function Header () {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const user = useSelector((state) => state.auth.user);
+
+  const handleClick = () => {
+    dispatch(doLogout());
+  };
+
   return (
     <nav className="main-nav">
       <NavLink to="/" className="main-nav-logo">
@@ -14,10 +26,25 @@ function Header() {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
-      <NavLink to="sign-in" className="main-nav-item">
-        <i className="fa fa-user-circle"></i>
-          Sign In
-      </NavLink>
+      <div className="separator"></div>
+      {
+        isAuthenticated 
+          ? 
+          <>
+            <div className="main-nav-item">
+              <span className="avatar"><SVGUser /></span>
+              <span className="name">{ user.firstName }</span>
+            </div>
+            <button className="main-nav-item" onClick={handleClick}>
+              <span className='signout'><SVGSignout /></span>
+              Sign Out
+            </button>
+          </>
+          : <NavLink to="sign-in" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </NavLink>
+      }
     </nav>
   );
 }
