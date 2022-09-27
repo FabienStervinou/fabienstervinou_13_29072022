39f35539d342142b7a11a01e3262bcaee2c70129
Fabien Stervinou <stervinoufabien@gmail.com>
2022-09-27 09:00:54 +0200

    <SignIn> create edit profil form and user logic

--- src/views/User/index.js
+++ src/views/User/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import store from '../../store/index';
+import { doChangeProfile } from '../../features/auth/auth.actions';
 import './style.scss';
 
-function User() {
+function User () {
+  const dispatch = useDispatch();
+  const state = store.getState();
+  const token = state.auth.token;
+  const user = state.auth.user;
+  const [toggleEdit, setToggleEdit] = useState(false);
+
+  const toggleEditForm = () => {
+    setToggleEdit(!toggleEdit);
+  }; 
+
+  const submitNewProfil = async (e) => {
+    e.preventDefault();
+    const firstname = e.target[0].value;
+    const lastname = e.target[1].value;
+
+    try {
+      await dispatch(doChangeProfile(firstname, lastname, token));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main className="main bg-dark">
       <div className="header">
-        <h1>Welcome back<br />Tony Jarvis!</h1>
-        <button className="edit-button">Edit Name</button>
+        {
+          !toggleEdit 
+            ? <>
+              <h1>Welcome back<br />{user.firstName} {user.lastName} !</h1>
+            </> 
+            : <>
+              <form onSubmit={submitNewProfil}>
+                <h1>Welcome back</h1>
+                <input name='firstName' type="text" placeholder={user.firstName}/>
+                <input name='lastName' type="text" placeholder={user.lastName}/>
+                <button className='edit-button'>Save</button>
+              </form>
+            </>
+        }
+
+        <button className="edit-button" onClick={() => toggleEditForm()}>
+          {!toggleEdit ? 'Edit Name' : 'Cancel'}
+        </button> 
+
       </div>
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
